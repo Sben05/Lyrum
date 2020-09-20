a8da89e43820701b502d3e19a49e2ff0022424f7
JOSH ARNOLD <jarnold@ucdavis.edu>
2020-09-19 21:56:51 -0700

    yas

--- lyrum/Backend/Player.swift
+++ lyrum/Backend/Player.swift
@@ -12,16 +12,9 @@ import Alamofire
 class SpotifyPlayer {
     
     static func playSong(song:Song) {
-        
-        print("Trying to req access token")
-
         SpotifyAPI.access_token { (success) in
-            
-            
-            
             if !success {
-                // passs
-                print("FAILED TO GET REQ ACCESS TOKEN")
+                // pass
             }else if let uuid = UIDevice.current.identifierForVendor?.uuidString {
                 
                 let access_token:String = SpotifyConstants.ACCESS_TOKEN
@@ -35,18 +28,9 @@ class SpotifyPlayer {
                     "device_id": uuid,
                     "uris":[song.uri],
                 ]
-                
-                print(parameters)
-                
-                print("About to make request...")
-                
+                                            
                 AF.request("https://api.spotify.com/v1/me/player/play", method: .put, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
-//                    print(response.result)
-//                    print(response.value)
-                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-                    print("RESULT:")
-                    print(response)
-                    print(response.value)
+                    // yas
                 }
             }
         }
